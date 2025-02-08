db17b55a53ac2812769065eebebfe6152ab88ce5
Shahd Hamdi <shahd.hamdi162@gmail.com>
2025-02-08 06:44:08 +0200

    Add Hive table creation scripts for CSV, Avro, and Parquet for Transfer Fact table

---
+++ HiveDWHExploration/LoadDataToHive/LoadTransferFactTable/LoadTransferParquetFact.sql
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE transfer_fact_parquet (
+    transfer_id INT,
+    player_id INT,
+    from_club_id INT,
+    to_club_id INT,
+    transfer_date DATE,
+    transfer_season STRING,  
+    transfer_fee FLOAT,
+    market_value_in_eur FLOAT,
+    highest_market_value_in_eur FLOAT,
+    player_age_at_transfer FLOAT,
+    transfer_profit_loss FLOAT,
+    transfer_fee_ratio FLOAT,
+    club_net_spend FLOAT,
+    from_club_goals INT,
+    from_club_assists INT, 
+    from_club_total_minutes INT
+)
+STORED AS PARQUET
+LOCATION 'hdfs://namenode/Star_schema/TransferFact_parquet';
